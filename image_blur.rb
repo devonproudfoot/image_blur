6deefb8dbd04b7d5fa18de9a9ba695d92a2bb1fc
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-07-24 18:00:32 +0000

    First attempt at image blur, grab index for 1s then try to change them

--- image_blur.rb
+++ image_blur.rb
@@ -11,6 +11,39 @@ class Image
     end
   end
 
+  def image_transform
+
+    # find each 1
+    ones = []
+    y = 0
+    while y < @image.length
+      @image.each do |row|
+        y += 1
+        if row.include?(1)
+          x = row.index(1) + 1
+          ones.push([x, y])
+        end
+      end
+    end
+
+    # do the actual transformation
+    ones.each do |one|
+      puts one.inspect
+      if !(one[0] = @image[0].length)
+        puts 'ok'
+        @image[one[1]][one[0 + 1]] = 1
+      elsif !(one[0] = 0)
+        @image[one[1]][one[0 - 1]] = 1
+      elsif not one[1] = @image.length
+        @image[one[1] + 1][one[0]] = 1
+      elsif not one[1] = 0
+        @image[one[1] - 1][one[0]] = 1
+      end
+
+    end
+
+  end
+
 end
 
 image = Image.new([
@@ -20,4 +53,7 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
+image.output_image
+image.image_transform
+puts '/'
 image.output_image
