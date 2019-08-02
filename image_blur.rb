52a9561481f92426e053a4db06d9d73bc010473d
Devon Proudfoot <proudfoot.devon@gmail.com>
2019-08-02 03:50:06 +0000

    created new blur function which will now transform based on distance

--- image_blur.rb
+++ image_blur.rb
@@ -23,50 +23,90 @@ class Image
     end
 
     return ones
-
   end
 
-  def image_transform
-
-    row_max = @image[0].length - 1
-    column_max = @image.length - 1
+  # replaced the image_transform method with blur, which will take the distance as an argument
+  def blur(distance)
+    row_max = @image[0].length() - 1
+    column_max = @image.length() - 1
 
     ones = self.get_ones
 
-    ones.each do |one|
-      vert = one[1]
-      horiz = one[0]
+    while distance > 0
+      ones.each do |one|
+        vert = one[1]
+        horiz = one[0]
+        # up 
+        unless vert + distance > column_max
+          @image[vert + distance][horiz] = 1
+        end
 
-      unless vert == column_max
-        @image[vert + 1][horiz] = 1
+        #down
+        unless vert - distance < 0
+          @image[vert - distance][horiz] = 1
+        end
+
+        # right
+        unless horiz + distance > row_max
+          @image[vert][horiz + distance] = 1
+        end
+
+        # left
+        unless horiz - distance < 0
+          @image[vert][horiz - distance] = 1
+        end
       end
 
-      unless vert == 0
-        @image[vert - 1][horiz] = 1
-      end
-
-      unless horiz == row_max
-        @image[vert][horiz + 1] = 1
-      end
-
-      unless horiz == 0
-        @image[vert][horiz - 1] = 1
-      end
-
+      distance -= 1
     end
+
   end
 
 end
 
-image = Image.new([
+# baby image
+image1 = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
   [0, 0, 1, 1],
   [0, 0, 0, 0]
 ])
 
-puts 'original image:'
-image.output_image
-image.image_transform
-puts 'new image:'
-image.output_image
+# rectangular image
+image2 = Image.new([
+  [0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0]
+])
+
+# monster image
+image3 = Image.new([
+  [0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 1, 0, 0]
+])
+
+puts 'original image 1:'
+image1.output_image
+image1.blur(2)
+puts 'new image 1:'
+image1.output_image
+
+puts 'original image 2:'
+image2.output_image
+image2.blur(2)
+puts 'new image 2:'
+image2.output_image
+
+puts 'original image 3:'
+image3.output_image
+image3.blur(2)
+puts 'new image 3:'
+image3.output_image
+
